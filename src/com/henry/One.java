673e7fbdbdb530b47fad93828a6b309c1c59b11c
Mqlvin <pricklydotcactus@gmail.com>
2021-11-08 21:28:05 +0000

    Initial commit.

---
+++ src/com/henry/One.java
@@ -0,0 +1,7 @@
+package com.henry;
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println("Result: " + String.valueOf((Math.pow(6, 2) - 6)).split("\\.")[0]);
+    }
+}
