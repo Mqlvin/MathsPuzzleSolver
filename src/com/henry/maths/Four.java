ccf714405dc471436a293fdfe5d0e05d62463400
Mqlvin <pricklydotcactus@gmail.com>
2021-12-02 10:16:33 +0000

    Added challenge four.

--- src/com/henry/maths/Four.java
+++ src/com/henry/maths/Four.java
@@ -3,7 +3,6 @@ package com.henry.maths;
 public class Four {
     public static void main(String[] args) {
         Integer day = 0;
-        Boolean answered = false;
         do {
             day++;
         } while(!(day % 7 == 0 && day % 6 == 0 && day % 5 == 0 && day % 4 == 0 && day % 3 == 0 && day % 2 == 0));
